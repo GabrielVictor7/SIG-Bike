569ab3c8eefbc4484492051e0ee05b98479acc76
danielmarizz7 <daniel.mariz.700@ufrn.edu.br>
2025-11-10 19:44:08 -0300

    Adiciona includes no módulo

--- src/relatorios.c
+++ src/relatorios.c
@@ -2,6 +2,10 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
+#include "../include/tela_inicial.h"
+#include "../include/relatorios.h"
+
+
 
 
 
@@ -14,14 +18,11 @@ char menu_relatorio(void) {
     printf("///                                                                       ///\n");                                                               
 	printf("/////////////////////////////////////////////////////////////////////////////\n");
 	printf("///                                                                       ///\n");
-	printf("///                                                                       ///\n");
 	printf("///           1. Relatório Clientes                                       ///\n");
 	printf("///           2. Relatório Funcionários                                   ///\n");
 	printf("///           3. Relatório De Bicicletas                                  ///\n");
 	printf("///           4. Relatório de Vendas                                      ///\n");
-	printf("///           0. Voltar ao menu anterior                                  ///\n");
-	printf("///                                                                       ///\n");
-	printf("///                                                                       ///\n");
+	printf("///           5. Voltar a tela anterior                                   ///\n");
 	printf("///                                                                       ///\n");
 	printf("/////////////////////////////////////////////////////////////////////////////\n");
     printf("Escolha uma opção: ");
