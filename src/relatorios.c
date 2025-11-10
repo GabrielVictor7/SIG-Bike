b24c81177f73472dbbaeec1739381e0c46bded4e
danielmarizz7 <daniel.mariz.700@ufrn.edu.br>
2025-11-10 20:05:03 -0300

    Refactor: Adiciona uma nova interface na tela

--- src/relatorios.c
+++ src/relatorios.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <ctype.h>
 #include "../include/tela_inicial.h"
 #include "../include/relatorios.h"
 
@@ -11,22 +10,19 @@
 
 char menu_relatorio(void) {
 	char op;
-	printf("\n");
-	printf("/////////////////////////////////////////////////////////////////////////////\n");
-	printf("///                                                                       ///\n");
-	printf("///                             Menu Relatório                            ///\n");
-    printf("///                                                                       ///\n");                                                               
-	printf("/////////////////////////////////////////////////////////////////////////////\n");
-	printf("///                                                                       ///\n");
-	printf("///           1. Relatório Clientes                                       ///\n");
-	printf("///           2. Relatório Funcionários                                   ///\n");
-	printf("///           3. Relatório De Bicicletas                                  ///\n");
-	printf("///           4. Relatório de Vendas                                      ///\n");
-	printf("///           5. Voltar a tela anterior                                   ///\n");
-	printf("///                                                                       ///\n");
-	printf("/////////////////////////////////////////////////////////////////////////////\n");
-    printf("Escolha uma opção: ");
-	scanf("%c", &op);
-	getchar();
-	return op;
+	system("clear||cls");
+        printf("╔═══════════════════════════════════════════════════════════════════════════════╗\n");
+        printf("║                                Módulo Relatório                               ║\n");
+        printf("╚═══════════════════════════════════════════════════════════════════════════════╝\n");
+        printf("║                           1. Lista Geral de Clientes                          ║\n");
+        printf("║                           2. Lista Geral de Bicicletas                        ║\n");
+        printf("║                           3. Lista Geral de Patinetes                         ║\n");
+        printf("║                           4. Bicicletas mais Alugadas                         ║\n");
+        printf("║                           5. Patinetes mais Alugados                          ║\n");
+        printf("║                                                                               ║\n");
+        printf("║                           0. Voltar                                           ║\n");
+        printf("╚═══════════════════════════════════════════════════════════════════════════════╝\n");
+        printf(" Digite a opção desejada: ");
+        scanf("%d", &op);
+        getchar();
 }
