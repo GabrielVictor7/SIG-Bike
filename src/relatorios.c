e199f5dc0f8b8a5ae15920f5e61c8a2ac95d0bea
danielmarizz7 <daniel.mariz.700@ufrn.edu.br>
2025-11-10 17:18:03 -0300

    Cria tela principal de relatório

--- src/relatorios.c
+++ src/relatorios.c
@@ -4,3 +4,28 @@
 #include <ctype.h>
 
 
+
+char menu_relatorio(void) {
+	char op;
+	printf("\n");
+	printf("/////////////////////////////////////////////////////////////////////////////\n");
+	printf("///                                                                       ///\n");
+	printf("///                             Menu Relatório                            ///\n");
+    printf("///                                                                       ///\n");                                                               
+	printf("/////////////////////////////////////////////////////////////////////////////\n");
+	printf("///                                                                       ///\n");
+	printf("///                                                                       ///\n");
+	printf("///           1. Relatório Clientes                                       ///\n");
+	printf("///           2. Relatório Funcionários                                   ///\n");
+	printf("///           3. Relatório De Bicicletas                                  ///\n");
+	printf("///           4. Relatório de Vendas                                      ///\n");
+	printf("///           0. Voltar ao menu anterior                                  ///\n");
+	printf("///                                                                       ///\n");
+	printf("///                                                                       ///\n");
+	printf("///                                                                       ///\n");
+	printf("/////////////////////////////////////////////////////////////////////////////\n");
+    printf("Escolha uma opção: ");
+	scanf("%c", &op);
+	getchar();
+	return op;
+}
