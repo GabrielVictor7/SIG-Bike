3fd6f650276b7579c9cf117a1a591a2bc5c9c6cc
danielmarizz7 <daniel.mariz.700@ufrn.edu.br>
2025-11-14 15:45:16 -0300

    Adiciona a chamada da função de relatório de funcionários ativos

--- include/relatorios.h
+++ include/relatorios.h
@@ -4,7 +4,7 @@
 void tela_relatorio_vendas(void);
 void relatorio_de_cliente_ativo(void);
 void relatorio_de_cliente_inativo(void);
-void relatorio_funcionario(void);
+void relatorio_de_funcionario_ativo(void);
 void relatorio_bicicletas(void);
 char menu_relatorio(void);
 void modulo_relatorios(void);
