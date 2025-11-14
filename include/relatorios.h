941e8cc7b128d03a872602ffb1f97ae5afec3e51
danielmarizz7 <daniel.mariz.700@ufrn.edu.br>
2025-11-14 15:54:49 -0300

    Adiciona chamada da função de funcionários inativos

--- include/relatorios.h
+++ include/relatorios.h
@@ -5,6 +5,7 @@ void tela_relatorio_vendas(void);
 void relatorio_de_cliente_ativo(void);
 void relatorio_de_cliente_inativo(void);
 void relatorio_de_funcionario_ativo(void);
+elatorio_de_funcionario_inativo(void);
 void relatorio_bicicletas(void);
 char menu_relatorio(void);
 void modulo_relatorios(void);
