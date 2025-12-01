584948063da9536cf6165974cc105f3821086286
danielmarizz7 <daniel.mariz.700@ufrn.edu.br>
2025-12-01 13:59:09 -0300

    Refactor: agora a função usa malloc

--- src/relatorios.c
+++ src/relatorios.c
@@ -325,6 +325,8 @@ void relatorio_de_cliente_inativo(void) {
     printf("ENTER para continuar");
     Enter();
 }
+
+
 void relat_clientes_ordem_alfabetica(void){
     FILE *fp = fopen(ARQ_CLIENTES, "rb");
     if (fp == NULL) {
@@ -478,56 +480,63 @@ void relatorio_de_funcionario_inativo(void) {
 
     // essa função foi totalmente reutilizada da anterior
     // já que segue a mesma ordem de filtro, a diferença so ocorre no while que testa se esta ativo ou inativo.
+     FILE *fp = fopen(ARQ_FUNCIONARIOS, "rb");
 
-FILE *fp = fopen(ARQ_FUNCIONARIOS, "rb");
-    
-    if (!fp) {
+    if (fp == NULL) {
         system("clear||cls");
         printf("--- Relatório de Funcionários ---\n");
-        printf("Nenhum funcionário cadastrado ainda.\n");
-        Enter(); 
+        printf("Erro na abertura dp arquivo\n");
+        printf("Enter para voltar...\n");
+        Enter();
         return;
     }
 
-    Funcionario func; 
-    int funcionarios_encontrados = 0;  
+    Funcionario *funcionario = malloc(MAX_FUNCIONARIO * sizeof(Funcionario));
+
+    int lidos = 0;
+    while (lidos < MAX_FUNCIONARIO &&
+           fread(&funcionario[lidos], sizeof(Funcionario), 1, fp) == 1) {
+        lidos++;
+    }
+
+    fclose(fp);
 
     system("clear||cls");
     printf("══════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════\n"); 
-    printf("════════════════════════════════════           Relatório Funcionário inativos         ════════════════════════════════════\n");
+    printf("═════════════════════════════════             Relatório Funcionários Ativos            ═══════════════════════════════════\n");
     printf("%-7s | %-30s | %-15s | %-40s | %-20s\n", 
            "Status", "Nome", "CPF", "Email", "Cargo");
-           
     printf("══════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════\n"); 
 
-    while (fread(&func, sizeof(Funcionario), 1, fp)) {
-        if (func.status == 'I') {
+    int ativos = 0;
+
+    for (int i = 0; i < lidos; i++) {
+        if (funcionario[i].status == 'I') {
             printf("%-7c | %-30s | %-15s | %-40s | %-20s\n",
-                   func.status,
-                   func.nome,    
-                   func.cpf,     
-                   func.email,   
-                   func.cargo); 
-            funcionarios_encontrados++;
+                   funcionario[i].status,
+                   funcionario[i].nome,
+                   funcionario[i].cpf,
+                   funcionario[i].email,
+                   funcionario[i].cargo);
+            ativos++;
         }
     }
-    
-    fclose(fp);
 
     printf("══════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════\n");
 
-    if (funcionarios_encontrados == 0) {
-        printf("\nNenhum funcionário inativo encontrado.\n");
-    } else {
-        printf("\nTotal de funcionarios inativos listados: %d\n", funcionarios_encontrados);
-    }
-    
-    printf("\nENTER para continuar");
-    getchar(); 
+    if (ativos == 0)
+        printf("Nenhum funcionário inativo encontrado.\n");
+    else
+        printf("Total de Funcionários inattivos listados: %d\n", ativos);
+    free(clientes);
+
+    printf("ENTER para continuar");
+    Enter();
 }
 
 
 void relat_funcionarios_ordem_alfabetica(void) {
+
     FILE* fp = fopen(ARQ_FUNCIONARIOS, "rb");
     if (!fp) {
         system("clear||cls");
